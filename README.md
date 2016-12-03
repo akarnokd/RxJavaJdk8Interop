b8b56db1a805cd2a9a8faf6d39ccc26c64ca0eaa
akarnokd <akarnokd@gmail.com>
2016-12-03 12:25:24 +0100

    Gradle setup

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # RxJava2Jdk8Interop
-RxJava 2 interop library for supporting Java 8 features such as Optional, Stream and CompletableFuture
+
+
+<a href='https://travis-ci.org/akarnokd/RxJava2Jdk8Interop/builds'><img src='https://travis-ci.org/akarnokd/RxJava2Jdk8Interop.svg?branch=master'></a>
+[![codecov.io](http://codecov.io/github/akarnokd/RxJava2Jdk8Interop/coverage.svg?branch=master)](http://codecov.io/github/akarnokd/RxJava2Jdk8Interop?branch=master)
+[![Maven Central](https://maven-badges.herokuapp.com/maven-central/com.github.akarnokd/rxjava2-jdk8-interop/badge.svg)](https://maven-badges.herokuapp.com/maven-central/com.github.akarnokd/rxjava2-jdk8-interop)
+
+
+RxJava 2 interop library for supporting Java 8 features such as Optional, Stream and CompletableFuture.
+
+# Release
+
+```groovy
+compile 'com.github.akarnokd:rxjava2-jdk8-interop:0.1.0'
+```
+
+# Examples
---
+++ src/main/java/hu/akarnokd/rxjava2/interop/RxJava2Jdk8Interop.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2016 David Karnok
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package hu.akarnokd.rxjava2.interop;
+
+/**
+ * Utility methods, sources and operators supporting RxJava 2 and the Jdk 8 API
+ * interoperation.
+ * 
+ * @since 0.1.0
+ */
+public final class RxJava2Jdk8Interop {
+
+    /** Utility class. */
+    private RxJava2Jdk8Interop() {
+        throw new IllegalStateException("No instances");
+    }
+}
