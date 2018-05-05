776c3285f6cf2ea38871d95e70042fe48359d1d8
Dávid Karnok <akarnokd@gmail.com>
2018-05-05 21:59:22 +0200

    Check out into not-new branch?

--- push.sh
+++ push.sh
@@ -4,6 +4,10 @@
 # ----------------------------------------------------------
 # based on https://gist.github.com/willprice/e07efd73fb7f13f917ea
 
+# specify the common address for the repository
+targetRepo=github.com/akarnokd/RxJava2Jdk8Interop.git
+# =======================================================================
+
 # only for main pushes, for now
 # if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
 if [ "$TRAVIS_PULL_REQUEST" == "true" ]; then
@@ -22,12 +26,12 @@ git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
 # setup the remote
-git remote add origin-pages https://${GITHUB_TOKEN}@github.com/akarnokd/RxJava2Jdk8Interop.git > /dev/null 2>&1
+git remote add origin-pages https://${GITHUB_TOKEN}@${targetRepo} > /dev/null 2>&1
 
 # get the gh-pages
 git fetch --all
 git branch -a
-git checkout -b gh-pages
+git checkout gh-pages
 
 # copy and overwrite new doc
 yes | cp -rfv ./build/docs/javadoc/ javadoc/
